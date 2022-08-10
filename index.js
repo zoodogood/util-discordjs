7cbe16eb8ed0d36e522e71f5dafbb64b6ca7a039
zoodogood <zoodogood@gmail.com>
2022-08-10 15:56:03 +0300

    init

---
+++ index.js
@@ -0,0 +1,116 @@
+import { ActionRow, Embed, ModalBuilder } from 'discord.js';
+import { ComponentType } from 'discord-api-types/v10';
+
+
+
+function isEmptyEmbed(embed){
+
+  const DEFAULT_PROPERTIES = {
+    title: null,
+    author: null,
+    thumbnail: null,
+    description: null,
+    fields: [],
+    image: null,
+    footer: null,
+    timestamp: Number.NaN,
+    video: null
+  }
+
+
+  // in isEmptyEmbed context is a good compare
+  const isEqualDefault = (key) => String(embed[key]) === String(DEFAULT_PROPERTIES[key]);
+
+  const isEveryPropertyDefault = Object.keys(DEFAULT_PROPERTIES)
+    .every(isEqualDefault);
+
+  return isEveryPropertyDefault;
+}
+
+
+function CreateMessage({
+  content,
+  title, url, author, thumbnail, description, color, fields, image, video, footer, timestamp,
+  ephemeral, fetchReply,
+  components, reactions, webhook,
+
+}){
+  const message = {};
+
+  thumbnail &&= { url: trumbnail };
+  image     &&= { url: image };
+
+  color ||= "RANDOM";
+
+  const embed = new Embed({
+    title, url, author, thumbnail,
+    description, color, fields,
+    image, video, footer, timestamp
+  });
+
+  if (!isEmptyEmbed(embed)){
+    message.embeds = [embed];
+  }
+
+  message.components = components ? new SimplifyComponents(components) : null;
+
+
+  message.content    = content;
+  message.ephemeral  = ephemeral;
+  message.fetchReply = fetchReply;
+
+  return message;
+}
+
+function CreateModal({title, customId, components}){
+  components = SimplifyComponents(components);
+  return new ModalBuilder().from({title, customId, components});
+}
+
+
+
+function SimplifyComponents(data){
+
+  if (data instanceof Array && data.length === 0)
+    return [];
+
+  const isComponent  = (component) => "type" in component;
+  const isActionRow  = (component) => component instanceof Array && isComponent(component.at(0)) || component instanceof MessageActionRow;
+  const isComponents = (component) => component.length && isActionRow(component.at(0));
+
+  const argumentType = [
+    isComponent(data),
+    isActionRow(data),
+    isComponents(data)
+  ].findIndex(Boolean);
+
+  if (argumentType === -1)
+    throw new TypeError("expected component");
+
+  const inArray = (component) => [component];
+  const arrayToActionRow = (componentsArray) => {
+    if (componentsArray.type === ComponentType.ActionRow)
+      return componentsArray;
+
+    return { type: ComponentType.ActionRow, components: componentsArray };
+  }
+  const actionRowInArray = (actionRow) => [actionRow];
+
+
+  if (argumentType <= 0)
+    data = inArray(data);
+
+  if (argumentType <= 1)
+    data = actionRowInArray(data)
+
+  data = data.map(arrayToActionRow);
+  return data;
+
+}
+
+export default {
+  CreateMessage,
+  CreateModal,
+  SimplifyComponents,
+  isEmptyEmbed
+};
